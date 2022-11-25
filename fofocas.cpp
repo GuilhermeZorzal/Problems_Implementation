cd7640d796c27538762d14cd8e05657f82767a41
Guilherme Broedel Zorzal <114156342+guilhermezorzal2004@users.noreply.github.com>
2022-11-25 09:26:03 -0300

    Implementação fofocas.cpp (secom)

---
+++ fofocas.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+
+    priority_queue <int> fila;
+    
+    int tam,cont = 0,x,y;
+    cin >> tam;
+
+    for (int i = 0; i<tam; i++){
+        x;
+        cin >> x;
+        if (x != 0) fila.push(x);
+    }
+    while(fila.size() > 1){
+        x = fila.top();
+        fila.pop();
+        y = fila.top();
+        fila.pop();
+        x = x-1;
+        y = y-1;
+        if (x != 0) fila.push(x);
+        if (y != 0) fila.push(y);
+        cont += 1;
+
+    }
+    cout << cont << endl;
+
+    return 0;
+}
+
